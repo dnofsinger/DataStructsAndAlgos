1f84e461e040e84e009f320af8d718b17751fd62
Derek Nofsinger <dereknofsinger@gmail.com>
2019-07-22 17:16:11 -0600

    Solved Euler Problem "Multiples of 3 and 5"

--- src/main/java/com/solutions/euler/AddMultiplesUtil.java
+++ src/main/java/com/solutions/euler/AddMultiplesUtil.java
@@ -1,8 +1,33 @@
 package com.solutions.euler;
 
+/* 
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000. 
+*/
+
 public class AddMultiplesUtil {
 
+    //function will take O(n)^2 time
     static int addMultiples(int[] multiples, int range){
-        return 0;
+
+        int sum = 0;
+        // Go through every number in the requested range O(n)
+        for( int i = 0; i < range; i++){
+
+            // Go through every multiple to see if we can divide by the number O(n)
+            for (int mult : multiples){
+
+                if(i % mult == 0)
+                {
+                    sum += i;
+                    break;
+                }
+
+            }
+
+        }
+
+        return sum;
+        
     }
 }
---
+++ src/test/java/com/solutions/euler/AddMultiplesUtilTest.java
@@ -0,0 +1,18 @@
+package com.solutions.euler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AddMultiplesUtilTest{
+
+    @Test
+    public void testAddMultiplesUtil(){
+
+        int[] mults = {3, 5};
+
+        assertEquals(23, AddMultiplesUtil.addMultiples(mults, 10));
+
+    }
+
+}
